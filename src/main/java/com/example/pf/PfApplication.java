40095686c6538ec2823daee28f1e094d320e7f63
ayoub <ayoub.oifik1@gmail.com>
2024-01-15 20:03:13 +0100

    4

--- src/main/java/com/example/pf/PfApplication.java
+++ src/main/java/com/example/pf/PfApplication.java
@@ -1,13 +1,17 @@
 package com.example.pf;
 
 import com.example.pf.entities.Pharmacie;
+import com.example.pf.entities.Role;
+import com.example.pf.entities.User;
 import com.example.pf.repo.PharmacieRepository;
+import com.example.pf.repo.UserRepository;
 import com.example.pf.repo.ZoneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 
@@ -29,4 +33,21 @@ public class PfApplication {
 //            System.out.println(pharmacie);
 //        };
 //    }
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Bean
+    CommandLineRunner init() {
+        return args -> {
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            User user = new User();
+            user.setUsername("admin@admin");
+            user.setEmail("admin@findpharma.com");
+            user.setRole(Role.ADMIN);
+            user.setPassword(passwordEncoder.encode("admin@admin"));
+            userRepository.save(user);
+
+        };
+    }
 }
